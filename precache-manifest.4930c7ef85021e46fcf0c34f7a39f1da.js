283bf845ea80fa8972e6367aabe4971600fffe8a
nnaing <nnaing@masonlive.gmu.edu>
2020-04-05 22:39:24 -0400

    Updates

---
+++ precache-manifest.4930c7ef85021e46fcf0c34f7a39f1da.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10fb072443ee0fd8f29596e2f6780fb",
+    "url": "/portfolio_2020/index.html"
+  },
+  {
+    "revision": "f4e9ef1d9778af9a4191",
+    "url": "/portfolio_2020/static/css/2.257c7e99.chunk.css"
+  },
+  {
+    "revision": "5a9de03b31d01a2049d1",
+    "url": "/portfolio_2020/static/css/main.b4d7c20f.chunk.css"
+  },
+  {
+    "revision": "f4e9ef1d9778af9a4191",
+    "url": "/portfolio_2020/static/js/2.70c37d05.chunk.js"
+  },
+  {
+    "revision": "f1b36842e5c8f99aac77a15a4681c3ef",
+    "url": "/portfolio_2020/static/js/2.70c37d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a9de03b31d01a2049d1",
+    "url": "/portfolio_2020/static/js/main.556e6a93.chunk.js"
+  },
+  {
+    "revision": "8dd04a533492d3e49e6c",
+    "url": "/portfolio_2020/static/js/runtime-main.70f8cb65.js"
+  },
+  {
+    "revision": "1d3ba590f1d23ade09bf3c078130ddb7",
+    "url": "/portfolio_2020/static/media/Nay_Resume.1d3ba590.pdf"
+  },
+  {
+    "revision": "c48373027b768cf29110cea4269c0c35",
+    "url": "/portfolio_2020/static/media/footer-pic.c4837302.gif"
+  },
+  {
+    "revision": "b44b14cf68d719ae7d08f6cf005467ba",
+    "url": "/portfolio_2020/static/media/portfolio-1.b44b14cf.png"
+  },
+  {
+    "revision": "92bb57b97dd859c8f79d18718a7d210e",
+    "url": "/portfolio_2020/static/media/portfolio-2.92bb57b9.png"
+  },
+  {
+    "revision": "b6d28e1cb95a6ea4e4dd620755a95d54",
+    "url": "/portfolio_2020/static/media/portfolio-3.b6d28e1c.png"
+  },
+  {
+    "revision": "748d9b47591cced1dacef623638930b2",
+    "url": "/portfolio_2020/static/media/portfolio-hero.748d9b47.jpg"
+  },
+  {
+    "revision": "deae66e6b91dc1c1e0db930c041976dc",
+    "url": "/portfolio_2020/static/media/portfolio-logo-2.deae66e6.png"
+  },
+  {
+    "revision": "3ad1112d7571342e7081d3c87eac3fb6",
+    "url": "/portfolio_2020/static/media/portfolio-logo-ext.3ad1112d.png"
+  },
+  {
+    "revision": "585fee66d4106c34e44a849b882e0d4a",
+    "url": "/portfolio_2020/static/media/portfolio-self.585fee66.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/portfolio_2020/precache-manifest.4930c7ef85021e46fcf0c34f7a39f1da.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio_2020/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
